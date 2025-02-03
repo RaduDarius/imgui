0e4194aff87cd1601327051589df2165a690eb86
DariusRadu <radu.va.darius@student.utcluj.ro>
2025-02-03 10:14:21 +0200

    Add lua script

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "imgui"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "imgui.h",
+        "imgui.cpp",
+
+        "imgui_internal.h",
+        "imconfig.h",
+        "imconfig.h",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+
+        "imgui_draw.cpp",
+        "imgui_demo.cpp",
+        "imgui_widgets.cpp",
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
